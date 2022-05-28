5abac84418de2f118063bed8a89a6d524c37aa06
unknown <danefes@gmail.com>
2022-05-28 16:59:27 +0300

    restructured app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import 'bootstrap';
 
-import Button from './components/UI/Button/Button';
-import AddUser from './components/Users/AddUser';
-import ConnectUser from './components/Users/ConnectUser';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
-  let [signUp, setSignUp] = useState(false);
-  let [signIn, setSignIn] = useState(false);
-
-  const signUpHandler = () => {
-    setSignUp(!signUp);
-  }
-  
-  const signInHandler = () => {
-    setSignIn(!signIn);
-  }
-
   return (
-    <div>
-      <div className='expenses'>
-        <div className="hub">
-          <span>Burger</span>
-          <span>hub</span>
-        </div>
-        {!signUp && !signIn? <Button className='white ml-1' onClick={signUpHandler}>Sign Up</Button> : ''}
-        {!signIn && !signUp ? <Button className='white ml-1' onClick={signInHandler}>Log In</Button> : ''}
-      </div>
-      {signUp ? <AddUser back={signUpHandler}></AddUser> : ''}
-      {signIn ? <ConnectUser back={signInHandler}></ConnectUser> : ''}
-    </div>
+    <Home />
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import 'bootstrap';
+
+import Button from '../UI/Button/Button';
+import AddUser from '../Users/AddUser';
+import ConnectUser from '../Users/ConnectUser';
+import './Home.css';
+
+function Home() {
+  let [signUp, setSignUp] = useState(false);
+  let [signIn, setSignIn] = useState(false);
+  let [loggedIn, setLoggedIn] = useState(false);
+
+  const signUpHandler = () => {
+    setSignUp(!signUp);
+  }
+  
+  const signInHandler = () => {
+    setSignIn(!signIn);
+  }
+
+  const loggedInHandler = () => {
+    setLoggedIn(!loggedIn);
+    setSignIn(false);
+    setSignUp(false);
+  }
+
+  return (
+    <React.Fragment>
+      <div className='expenses'>
+        <div className="hub">
+          <span>Burger</span>
+          <span>hub</span>
+        </div>
+        {!signUp && !signIn && !loggedIn ? <Button className='white ml-1' onClick={signUpHandler}>Sign Up</Button> : ''}
+        {!signIn && !signUp && !loggedIn ? <Button className='white ml-1' onClick={signInHandler}>Log In</Button> : ''}
+        {loggedIn ? <Button className='white ml-1' onClick={loggedInHandler}>Log Out</Button> : ''}
+      </div>
+      {signUp ? <AddUser back={signUpHandler}></AddUser> : ''}
+      {signIn ? <ConnectUser back={signInHandler} success={loggedInHandler}></ConnectUser> : ''}
+    </React.Fragment>
+  );
+}
+
+export default Home;
