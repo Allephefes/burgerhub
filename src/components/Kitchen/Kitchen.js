4508444302d57b5c97e49345e58619c1960b5780
unknown <danefes@gmail.com>
2022-06-20 17:03:39 +0300

    basic kitchen page

--- src/components/Kitchen/Kitchen.js
+++ src/components/Kitchen/Kitchen.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../Header/Header';
-import Button from '../UI/Button/Button';
+import CartIcon from './Cart/CartIcon';
+import Cart from './Cart/Cart';
 import KitchenHeader from '../../assets/burger-header.jpg';
 import Products from '../Products/Products';
 
-const Kitchen = (props) => {
-    return <React.Fragment>
+import ProductProvider from '../../context/Providers/ProductProvider';
+
+const Kitchen = () => {
+    const [cart, setCart] = useState(false);
+
+    const cartIconHandler = () => {
+        setCart(!cart);
+    }
+
+    return <ProductProvider>
         <Header>
-            <Button>Cart</Button>
+            <CartIcon onClick={cartIconHandler}></CartIcon>
         </Header>
         <div className='main-image'>
             <img src={KitchenHeader} alt='Table full of YUMMY!'></img>
         </div>
+        {cart ? <Cart close={cartIconHandler}></Cart> : ''}
         <Products></Products>
-    </React.Fragment>
+    </ProductProvider>
 }
 
 export default Kitchen;
