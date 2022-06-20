25e82b71d188249906bd7a5bdee56a622b243ef8
unknown <danefes@gmail.com>
2022-06-20 17:00:39 +0300

    added admin page

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+import AuthUser from '../Users/AuthUser';
+
+const Admin = () => {
+    return <div>
+        <Header />
+        <AuthUser />
+    </div>
+}
+
+export default Admin;
