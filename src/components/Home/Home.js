cb600797b493130f390b27792b7cc26a139b0840
unknown <danefes@gmail.com>
2022-06-20 17:01:27 +0300

    added auth button in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,10 +11,14 @@ function Home(props) {
     let [signUp, setSignUp] = useState(false);
     let [signIn, setSignIn] = useState(false);
     let [loggedIn, setLoggedIn] = useState(false);
+    let [userAuth, setUserAuth] = useState(false);
 
     useEffect(() => {
         if (localStorage.getItem('logged_in')) {
             setLoggedIn(true);
+        }
+        if (localStorage.getItem('auth')) {
+            setUserAuth(true);
         }
     }, []);
 
@@ -26,16 +30,22 @@ function Home(props) {
         setSignIn(!signIn);
     }
 
-    const loggedInHandler = () => {
-        localStorage.setItem('logged_in', true);
+    const loggedInHandler = (auth) => {
         setLoggedIn(true);
         setSignIn(false);
         setSignUp(false);
+        localStorage.setItem('logged_in', true);
+        if (auth === 'admin') {
+            setUserAuth(true);
+            localStorage.setItem('auth', true);
+        }
     }
 
     const loggedOutHandler = () => {
         localStorage.removeItem('logged_in');
+        localStorage.removeItem('auth');
         setLoggedIn(false);
+        setUserAuth(false);
     }
 
     return (
@@ -45,11 +55,14 @@ function Home(props) {
                     {!signUp && !signIn && !loggedIn ? <Button className='white ml-1' onClick={signUpHandler}>Sign Up</Button> : ''}
                     {!signIn && !signUp && !loggedIn ? <Button className='white ml-1' onClick={signInHandler}>Log In</Button> : ''}
                 </div>
-                {loggedIn ? <Button className='white ml-1' onClick={loggedOutHandler}>Log Out</Button> : ''}
+                <div>
+                    {userAuth ? <Link to='/admin' style={{ color: 'inherit', textDecoration: 'inherit' }}><Button className='white ml-1'>Admin</Button></Link> : ''}
+                    {loggedIn ? <Button className='white ml-1' onClick={loggedOutHandler}>Log Out</Button> : ''}
+                </div>
             </Header>
             {signUp ? <AddUser back={signUpHandler}></AddUser> : ''}
             {signIn ? <ConnectUser back={signInHandler} success={loggedInHandler}></ConnectUser> : ''}
-            {loggedIn ? <Button className='white ml-1'><Link to='/kitchen' style={{ color: 'inherit', textDecoration: 'inherit'}}>Shop</Link></Button> : ''}
+            {loggedIn ? <Button className='white ml-1'><Link to='/kitchen' style={{ color: 'inherit', textDecoration: 'inherit' }}>Shop</Link></Button> : ''}
         </React.Fragment>
     );
 }
