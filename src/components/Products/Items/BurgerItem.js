50d2c9a0772d8ef7be23f1451c2f03694d554806
unknown <danefes@gmail.com>
2022-06-23 18:59:59 +0300

    changed burger-item

--- src/components/Products/Items/BurgerItem.js
+++ src/components/Products/Items/BurgerItem.js
@@ -3,13 +3,17 @@ import React from 'react';
 const BurgerItem = (props) => {
     const price = props.item.price;
     const burger = props.item;
+
     return <React.Fragment>
-        <div className='fdir-row'>
-            <div>Bun: {burger.bun.name !== '' ? burger.bun.name : 'None'}</div>
-            <div>Meat: {burger.meat.name !== '' ? `${burger.meat.name} x ${burger.meat.amount}` : 'None'}</div>
-            <div>Vegetables: {burger.veggies.length !== 0 ? burger.veggies.map((v) => {return `${v.name} `}) : 'None'}</div>
-            <div>Topping: {burger.topping.name !== '' ? burger.topping.name : 'None'}</div>
-            <div>Sauce: {burger.sauce.length !== 0 ? burger.sauce.map((s) => {return `${s.name} `}) : 'None'}</div>
+        <div className='fdir-column'>
+            <div className='fdir-row'>
+                <div className='item'>Bun: {burger.bun.name !== '' ? burger.bun.name : '-'}</div>
+                <div className='item'>Meat: {burger.meat.name !== '' ? `${burger.meat.name} x ${burger.meat.amount}` : '-'}</div>
+                <div className='item'>Vegetables: {burger.veggies.length !== 0 ? burger.veggies.map((v) => { return `${v.name} ` }) : '-'}</div>
+                <div className='item'>Topping: {burger.topping.name !== '' ? burger.topping.name : '-'}</div>
+                <div className='item'>Sauce: {burger.sauce.length !== 0 ? burger.sauce.map((s) => { return `${s.name} ` }) : '-'}</div>
+            </div>
+            {props.children}
         </div>
         <h3>
             {price !== 0 ? `$${price}` : ''}
