bac60c5563dd51e152e4d96877465b516808d0ba
unknown <danefes@gmail.com>
2022-06-23 19:00:28 +0300

    fixed issues with rendering in product-item

--- src/components/Products/Items/ProductItem.js
+++ src/components/Products/Items/ProductItem.js
@@ -6,19 +6,35 @@ import Button from '../../UI/Button/Button';
 const ProductItem = (props) => {
     const ctx = useContext(ProductContext);
 
+    let usedProduct = ctx.getItem(props.part, props.name);
+    const price = `$${props.price}`
+
     const setItem = (event) => {
         event.preventDefault();
-        ctx.setItem(props.part, {name: props.name, price: props.price})
+        ctx.setItem(props.part, { name: props.name, price: props.price })
     }
 
-    const price = `$${props.price}`
+    const removeItem = (event) => {
+        event.preventDefault();
+        ctx.removeItem(props.part);
+    }
 
     return <li className='meal'>
         <div>
             <h3>{props.name}</h3>
             <div>{price}</div>
         </div>
-        <Button className='form' onClick={setItem}>{ctx.currentBurger[props.part].name === '' || (props.part === 'meat' && props.name === ctx.currentBurger[props.part].name) || /veggies|sauce/.test(props.part) ? '+ Add' : 'Change'}</Button>
+        {usedProduct ?
+            <Button className='form' onClick={removeItem}>
+                Remove
+            </Button> : ''
+        }
+        {(props.name === ctx.currentBurger.meat.name && ctx.currentBurger.meat.amount <= 4) || !usedProduct ?
+            <Button className='form' onClick={setItem}>
+                {ctx.currentBurger[props.part].name === '' || usedProduct || /veggies|sauce/.test(props.part) ? '+ Add' : 'Change'}
+            </Button> : 
+            ''
+        }
     </li>
 }
 
